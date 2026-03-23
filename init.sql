981e5ee891f46cfcb6e986445626d362a0830c4c
LUNA2226 <oliveiraluziana964@gmail.com>
2026-03-23 11:24:09 -0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,12 @@
+-- Extensões para o Georadar (Mapas e Rotas)
 CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
+CREATE EXTENSION IF NOT EXISTS pgrouting;
+
+-- Extensões para o Chatwoot (Busca e Identidade)
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS unaccent;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Extensão para o Cérebro do Chatwoot (IA/Vetores)
+CREATE EXTENSION IF NOT EXISTS vector;
